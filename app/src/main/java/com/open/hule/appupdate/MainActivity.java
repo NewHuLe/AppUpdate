1c8c68400196dec1c637b976560de8c39bc6b7ed
hule <hule@mysteel.com>
2019-12-02 09:38:55 +0800

    更新代码示例

--- app/src/main/java/com/open/hule/appupdate/MainActivity.java
+++ app/src/main/java/com/open/hule/appupdate/MainActivity.java
@@ -57,15 +57,21 @@ public class MainActivity extends AppCompatActivity implements MainPageExtraList
         AppUpdate appUpdate = new AppUpdate.Builder()
                 //更新地址（必传）
                 .newVersionUrl("https://imtt.dd.qq.com/16891/apk/5CACCB57E3F02E46404D27ABAA85474C.apk")
-                // 版本号
+                // 版本号（非必填）
                 .newVersionCode("v1.4")
-                // 通过传入资源id来自定义更新对话框，注意取消更新的id要定义为btnUpdateLater，立即更新的id要定义为btnUpdateNow
+                // 通过传入资源id来自定义更新对话框，注意取消更新的id要定义为btnUpdateLater，立即更新的id要定义为btnUpdateNow（非必填）
                 .updateResourceId(R.layout.dialog_update)
-                // 文件大小
+                // 更新的标题，弹框的标题（非必填，默认为应用更新）
+                .updateTitle(R.string.update_title)
+                // 更新内容的提示语，内容的标题（非必填，默认为更新内容）
+                .updateContentTitle(R.string.update_content_lb)
+                // 更新内容（非必填，默认“1.用户体验优化\n2.部分问题修复”）
+                .updateInfo("1.用户体验优化\n2.部分问题修复")
+                // 文件大小（非必填）
                 .fileSize("5.8M")
-                //是否采取静默下载模式（只显示更新提示，后台下载完自动弹出安装界面），否则，显示下载进度，显示下载失败弹框
+                //是否采取静默下载模式（非必填，只显示更新提示，后台下载完自动弹出安装界面），否则，显示下载进度，显示下载失败
                 .isSilentMode(true)
-                //默认不采取强制更新，否则，不更新无法使用
+                //是否强制更新（非必填，默认不采取强制更新，否则，不更新无法使用）
                 .forceUpdate(0)
                 .build();
         updateManager.startUpdate(appUpdate, this);
@@ -128,7 +134,7 @@ public class MainActivity extends AppCompatActivity implements MainPageExtraList
     }
 
     /**
-     *  授权后，再次尝试安装
+     * 授权后，再次尝试安装
      */
     private void installApkAgain() {
         if (updateManager != null) {
